86f600cbed4dd110e6e2a94aaa905c2a31431390
Steele <scstonick@gmail.com>
2023-01-31 22:16:17 -0500

    updated readme and added info to variables file

--- variables.tf
+++ variables.tf
@@ -1,26 +1,37 @@
 
 variable "api_name" {
+  type = string
+  description = "This will be the name of the AWS API Gateway"
   # default = "examplegateway"
 }
 
 variable "resource_names" {
+  type = list(string)
+  description = "This is an array of resource names you want to make in the AWS API Gateway"
   # default = ["exampleresource1", "exampleresource2"]
 }
 
 variable "stage_name" {
+  type = string
+  description = "This will be the name of the gateway's stage"
   # default = "init"
 }
 
 variable "unanimous_layers" {
+  type = list(string)
+  description = "This is an array of ARNs of lambda layer that you want to be integrated all lambda functions this module creates"
   
 }
 
 variable "user_pool_auth" {
   type = number
+  description = "This is a sort of boolean for integrating auth with the API being created. Set to 0 to not use auth and set to 1 in order to use auth. If using authenticaiton you must also provide a cognitio user pool ARN in the variable 'cognito_user_pool_arn'."
   default = 0
 }
 
 variable "cognito_user_pool_arn" {
+  type = string
+  description = "If 'user_pool_auth' is set to 1 then this variable must be set to an AWS Cognito user pool's ARN. If not using auth then ignore this."
   default = ""
 }
 
@@ -36,4 +47,5 @@ variable "functions" {
         resource_name = string
         layers = list(string)
   }))
+  description = "this variable takes a list of objects. Each object will generate a lambda function and a method in the API Gateway that integrates that lambda function"
 }
